7dbfb68796dab31718d218c92972753c723f9120
SUNILmobcoder <sunil.mobcoder@gmail.com>
2021-12-20 12:31:11 +0530

    initial template

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <React.Fragment>
       <div className="container mx-auto">
         <h3 className="text-center text-3xl mt-20 leading-8 text-black font-bold tracking-wide uppercase">
-          CRUD with React Context API and Hooks
+          CRUD with React
         </h3>
         <Heading />
         <EmployeeList />
